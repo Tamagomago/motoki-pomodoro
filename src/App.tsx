f9a01ea2a4303ea89c7e59aaf61279488c1fbd06
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-06-02 10:45:27 +0800

    chore: setup shadcn/ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
 function App() {
   return (
     <>
       <h1 className={'text-2xl text-blue-500'}>TEST</h1>
+      <Button>Test Button</Button>
     </>
   );
 }
